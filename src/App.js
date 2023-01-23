d38bdef4e48e009d48b98d0651a5c480eedfd672
Anugya Ram <anugyaram08@gmail.com>
2023-01-23 20:09:14 +0530

    router config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import React from 'react';
+import { Route,Routes,BrowserRouter } from 'react-router-dom';
+import AddEditBlog from './pages/AddEditBlog';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/addBlog' element={<AddEditBlog />}></Route>
+      <Route path='/editBlog/:id' element={<AddEditBlog />}></Route>
+      <Route path='/about' element={<About />}></Route>
+      <Route path='/blog/:id' element={<Blog />}></Route>
+      <Route path='*' element={<NotFound />}></Route>
+    </Routes>
+    </BrowserRouter>
+
   );
 }
 
