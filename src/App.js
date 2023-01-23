17dc31e7213362a6c51fe9fb71ff4572d56d9863
Anugya Ram <anugyaram08@gmail.com>
2023-01-23 22:00:10 +0530

    server setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import React from 'react';
+
 import { Route,Routes,BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.min.css';
+
+
 import AddEditBlog from './pages/AddEditBlog';
 import NotFound from './pages/NotFound';
 import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
 
+
 function App() {
   return (
     <BrowserRouter>
+    <ToastContainer />
     <Routes>
       <Route path='/' element={<Home />}></Route>
       <Route path='/addBlog' element={<AddEditBlog />}></Route>
